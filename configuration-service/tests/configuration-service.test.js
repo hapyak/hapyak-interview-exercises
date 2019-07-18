d0070d57ed872695bafbcdc589ec742d5cf60839
David <dbernstein@hapyak.com>
2019-07-18 11:46:00 -0400

    Remove benchmark library.

--- configuration-service/tests/configuration-service.test.js
+++ configuration-service/tests/configuration-service.test.js
@@ -1,6 +1,5 @@
 const configurationService = require('../configuration-service');
 const fixture = require('./configuration-service-fixtures.json');
-const Benchmark = require('benchmark');
 
 test('resolveNestedConfigurartion', () => {
     resolvedConfiguration = configurationService.getResolvedConfiguration(201);
